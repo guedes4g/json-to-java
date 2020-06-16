8d5876c62ef79c8cbc6d19494b637152ad12cb2d
Mateus Haas <mateus@mhaas.com.br>
2020-06-15 23:11:36 -0300

    Add IO to unique file

---
+++ src/io.ts
@@ -0,0 +1,31 @@
+import fs = require("fs");
+import path = require("path");
+
+import { javaClassBody, javaClass } from "./types";
+import writeJavaClass from "./handleJava";
+import jsonToJava from "./handleJson";
+
+export const writeJavaClasses = (classes: javaClass[], dirPath: string): void => {
+  classes.forEach(javaClass => {
+    const { name, textBody }: javaClassBody = writeJavaClass(javaClass);
+
+    fs.writeFileSync(path.join(dirPath, `${name}.java`), textBody);
+  });
+};
+
+export const openJsonFile = (filePath: string): javaClass[] | null => {
+  const fileBody = fs.readFileSync(filePath, "utf8");
+  const fileName = path.basename(filePath);
+
+  console.log(fileName);
+
+  try {
+    const jsonRawData = JSON.parse(fileBody);
+
+    return jsonToJava(jsonRawData, fileName);
+  }
+  catch (ex) {
+    console.error("Error parsing file to JSON.");
+    return null;
+  }
+}
